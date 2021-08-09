65f5d5ca2436cf91d7e33760d3498e64919a3241
Ryuta Udo <ryuta.udo@brands4friends.jp>
2021-08-09 17:00:17 +0900

    WIP error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,71 @@
+use std::str::Utf8Error;
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+        unimplemented!()
+    } 
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocaol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocaol => "Invalid Protocaol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+      write!(f, "{}", self.message())
+  }
+}
+
+impl Debug for ParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+      write!(f, "{}", self.message())
+  }
+}
+
+impl Error for ParseError {
+
 }
